b4dd65afaab1ae10c70e68df04e403819b44f7a9
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-25 13:45:28 -0700

    Uncomment g.template_engine :haml in lib/planet_express/engine.rb

--- PlanetExpress/lib/planet_express/engine.rb
+++ PlanetExpress/lib/planet_express/engine.rb
@@ -2,7 +2,7 @@ module PlanetExpress
   class Engine < ::Rails::Engine
     isolate_namespace PlanetExpress
      config.generators do |g|
-       #g.template_engine  :haml
+       g.template_engine  :haml
        g.test_framework   :rspec, fixture: false
        g.integration_tool :rspec
        g.fixture_replacement :factory_girl, dir: 'spec/factories'
