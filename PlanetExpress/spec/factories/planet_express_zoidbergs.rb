bbdf7299e7792b9be0b4398882b098af84cea82c
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-26 13:56:12 -0700

    Remove one of the Zoidberg factories

--- PlanetExpress/spec/factories/planet_express_zoidbergs.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :planet_express_zoidberg, :class => 'Zoidberg' do
-    name "MyString"
-  end
-end
