d11cdf46eea6703aa1858884a2f01236cc3c1e0a
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-26 11:03:14 -0700

    Set ORM to mongoid in lib/planet_express/engine.rb

--- PlanetExpress/lib/planet_express/engine.rb
+++ PlanetExpress/lib/planet_express/engine.rb
@@ -2,6 +2,7 @@ module PlanetExpress
   class Engine < ::Rails::Engine
     isolate_namespace PlanetExpress
      config.generators do |g|
+       g.orm              :mongoid
        g.template_engine  :haml
        g.test_framework   :rspec, fixture: false
        g.integration_tool :rspec
